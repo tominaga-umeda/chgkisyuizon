ae68b63b5e909a6d0d272a0be1bef1632433f153
tominaga-umeda <umeda@kusuriya.jp>
2026-01-22 16:22:14 +0900

    新規作成

---
+++ Unit1.dfm
@@ -0,0 +1,273 @@
+object Form1: TForm1
+  Left = 179
+  Top = 88
+  Width = 1201
+  Height = 675
+  Caption = #27231#31278#20381#23384#25991#23383#22793#25563
+  Color = clBtnFace
+  Font.Charset = SHIFTJIS_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -13
+  Font.Name = 'Meiryo UI'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  OnResize = FormResize
+  PixelsPerInch = 96
+  TextHeight = 17
+  object Memo1: TMemo
+    Left = 0
+    Top = 346
+    Width = 82
+    Height = 290
+    Align = alLeft
+    ScrollBars = ssVertical
+    TabOrder = 0
+    WordWrap = False
+  end
+  object Memo2: TMemo
+    Left = 82
+    Top = 346
+    Width = 82
+    Height = 290
+    Align = alLeft
+    ScrollBars = ssVertical
+    TabOrder = 1
+    WordWrap = False
+  end
+  object Panel1: TPanel
+    Left = 164
+    Top = 346
+    Width = 1021
+    Height = 290
+    Align = alClient
+    BevelOuter = bvNone
+    TabOrder = 2
+    object LtBF: TListBox
+      Left = 0
+      Top = 0
+      Width = 522
+      Height = 290
+      Align = alLeft
+      ItemHeight = 17
+      TabOrder = 0
+    end
+    object LtAF: TListBox
+      Left = 522
+      Top = 0
+      Width = 499
+      Height = 290
+      Align = alClient
+      ItemHeight = 17
+      TabOrder = 1
+    end
+  end
+  object Panel2: TPanel
+    Left = 0
+    Top = 0
+    Width = 1185
+    Height = 182
+    Align = alTop
+    BevelOuter = bvNone
+    TabOrder = 3
+    DesignSize = (
+      1185
+      182)
+    object SpeedButton1: TSpeedButton
+      Left = 380
+      Top = 2
+      Width = 46
+      Height = 53
+      Caption = #22793#25563
+      OnClick = SpeedButton1Click
+    end
+    object SpeedButton2: TSpeedButton
+      Left = 199
+      Top = 2
+      Width = 143
+      Height = 53
+      Caption = #27231#31278#20381#23384#25991#23383#34892#34920#31034
+      OnClick = SpeedButton2Click
+    end
+    object SpeedButton3: TSpeedButton
+      Left = 13
+      Top = 2
+      Width = 146
+      Height = 25
+      Caption = #12463#12522#12483#12503#12508#12540#12489#12363#12425#36028#12426#20184#12369
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clGreen
+      Font.Height = -13
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+      OnClick = SpeedButton3Click
+    end
+    object SpeedButton4: TSpeedButton
+      Left = 13
+      Top = 30
+      Width = 146
+      Height = 25
+      Caption = #12467#12500#12540#12375#12383#12501#12449#12452#12523#12434#34920#31034
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clRed
+      Font.Height = -13
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+      OnClick = SpeedButton4Click
+    end
+    object Label1: TLabel
+      Left = 170
+      Top = 2
+      Width = 21
+      Height = 26
+      Caption = #8658
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clGreen
+      Font.Height = -21
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Label2: TLabel
+      Left = 350
+      Top = 15
+      Width = 21
+      Height = 26
+      Caption = #8658
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -21
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Label3: TLabel
+      Left = 433
+      Top = 2
+      Width = 21
+      Height = 26
+      Caption = #8658
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clGreen
+      Font.Height = -21
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object SpeedButton5: TSpeedButton
+      Left = 461
+      Top = 2
+      Width = 146
+      Height = 25
+      Caption = #12463#12522#12483#12503#12508#12540#12489#12395#12467#12500#12540
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clGreen
+      Font.Height = -13
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+      OnClick = SpeedButton5Click
+    end
+    object SpeedButton6: TSpeedButton
+      Left = 461
+      Top = 30
+      Width = 146
+      Height = 25
+      Caption = #12501#12449#12452#12523#12395#19978#26360#12365#20445#23384
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clRed
+      Font.Height = -13
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+      OnClick = SpeedButton6Click
+    end
+    object Label4: TLabel
+      Left = 170
+      Top = 31
+      Width = 21
+      Height = 26
+      Caption = #8658
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clRed
+      Font.Height = -21
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Label5: TLabel
+      Left = 433
+      Top = 31
+      Width = 21
+      Height = 26
+      Caption = #8658
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clRed
+      Font.Height = -21
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object SpeedButton7: TSpeedButton
+      Left = 13
+      Top = 66
+      Width = 201
+      Height = 25
+      Caption = #12467#12500#12540#12375#12383#12501#12457#12523#12480#20869#12398#12501#12449#12452#12523#12434#26908#32034
+      OnClick = SpeedButton7Click
+    end
+    object Label6: TLabel
+      Left = 266
+      Top = 59
+      Width = 24
+      Height = 30
+      Caption = #8593
+      Font.Charset = SHIFTJIS_CHARSET
+      Font.Color = clRed
+      Font.Height = -24
+      Font.Name = 'Meiryo UI'
+      Font.Style = []
+      ParentFont = False
+    end
+    object LtFiles: TListBox
+      Left = 29
+      Top = 98
+      Width = 1142
+      Height = 78
+      Anchors = [akLeft, akTop, akRight]
+      ItemHeight = 17
+      TabOrder = 0
+      OnClick = LtFilesClick
+    end
+  end
+  object Panel3: TPanel
+    Left = 0
+    Top = 182
+    Width = 1185
+    Height = 164
+    Align = alTop
+    BevelOuter = bvNone
+    TabOrder = 4
+    object ListBox1: TListBox
+      Left = 0
+      Top = 0
+      Width = 1062
+      Height = 164
+      Align = alClient
+      ItemHeight = 17
+      TabOrder = 0
+      OnClick = ListBox1Click
+    end
+    object ListBox2: TListBox
+      Left = 1062
+      Top = 0
+      Width = 123
+      Height = 164
+      Align = alRight
+      ItemHeight = 17
+      TabOrder = 1
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,250 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, Buttons, Clipbrd, ShellApi, FileCtrl;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    Memo2: TMemo;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    Panel3: TPanel;
+    ListBox1: TListBox;
+    ListBox2: TListBox;
+    LtBF: TListBox;
+    LtAF: TListBox;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    Label4: TLabel;
+    Label5: TLabel;
+    SpeedButton7: TSpeedButton;
+    LtFiles: TListBox;
+    Label6: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure SpeedButton2Click(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure SpeedButton3Click(Sender: TObject);
+    procedure SpeedButton4Click(Sender: TObject);
+    procedure ListBox1Click(Sender: TObject);
+    procedure SpeedButton5Click(Sender: TObject);
+    procedure SpeedButton6Click(Sender: TObject);
+    procedure SpeedButton7Click(Sender: TObject);
+    procedure LtFilesClick(Sender: TObject);
+  private
+    { Private 宣言 }
+    g_saFileName: AnsiString;
+  public
+    { Public 宣言 }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  sl, sll: TStringList;
+  iw: Word;
+begin
+  sl := TStringList.Create;
+  sll := TStringList.Create;
+  try
+    sl.LoadFromFile(ExtractFilePath(Application.ExeName) + '変換.csv');
+    for iw := 0 to Pred(sl.Count) do begin
+      sll.CommaText := sl[iw];
+      Memo1.Lines.Add(sll[0]);
+      Memo2.Lines.Add(sll[1]);
+    end;
+  finally
+    sl.Free;
+    sll.Free;
+  end;
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  LtBF.Width := Panel1.Width div 2;
+end;
+
+procedure TForm1.SpeedButton2Click(Sender: TObject);
+var
+  iw1, iw2: Word;
+  sa1, sa2: AnsiString;
+begin
+  ListBox1.Items.Clear;
+  ListBox2.Items.Clear;
+
+  for iw1 := 0 to Pred(LtBF.Items.Count) do begin
+    sa1 := LtBF.Items[iw1];
+    for iw2 := 0 to Pred(Memo1.Lines.Count) do begin
+      sa2 := Memo1.Lines[iw2];
+      if AnsiPos(sa2, sa1) <> 0 then begin
+        ListBox1.Items.Add(sa1);
+        ListBox2.Items.Add(IntToStr(iw1));
+      end;
+    end;
+  end;
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+var
+  iw1, iw2: Word;
+  sa1, sa2: AnsiString;
+begin
+  LtAF.Items.Clear;
+  LtAF.Visible := False;
+  Application.ProcessMessages;
+  try
+    for iw1 := 0 to Pred(LtBF.Items.Count) do begin
+      sa1 := LtBF.Items[iw1];
+      for iw2 := 0 to Pred(Memo1.Lines.Count) do begin
+        sa2 := Memo1.Lines[iw2];
+        if Pos(sa2, sa1) <> 0 then begin
+          sa1 := StringReplace(sa1, sa2, Memo2.Lines[iw2], [rfReplaceAll]);
+        end;
+      end;
+      LtAF.Items.Add(sa1);
+    end;
+  finally
+    LtAF.Visible := True;
+  end;
+end;
+
+procedure TForm1.SpeedButton3Click(Sender: TObject);
+var
+  i : integer;
+  S : string;
+begin
+  //クリップボードの内容がテキストだったら、
+  if Clipboard.HasFormat(CF_TEXT) then begin
+    //クリップボードのサイズ取得
+    i := GlobalSize(Clipboard.GetAsHandle(CF_TEXT));
+    SetLength(S, i);
+    //クリップボードの内容取得
+    Clipboard.GetTextBuf(PChar(S), i);
+    SetLength(S, StrLen(PChar(S)));
+    //メモに表示
+    LtBF.Items.Text := S;
+  end;
+end;
+
+procedure TForm1.SpeedButton4Click(Sender: TObject);
+var
+  i, iFileCount : integer;
+  hDrop : THandle;
+  szFile : array[0..MAX_PATH - 1] of Char;
+begin
+  if Clipboard.HasFormat(CF_HDROP) then begin
+    hDrop := Clipboard.GetAsHandle(CF_HDROP);
+    iFileCount := DragQueryFile(hDrop, $FFFFFFFF, szFile, MAX_PATH);
+    //ShowMessage('ファイル数＝' + IntToStr(iFileCount));
+
+    //for i := 0 to iFileCount - 1 do begin
+      DragQueryFile(hDrop, 0, szFile, MAX_PATH);
+      g_saFileName := szFile;
+      LtBF.Items.LoadFromFile(g_saFileName);
+    //end;
+  end;
+end;
+
+procedure TForm1.ListBox1Click(Sender: TObject);
+begin
+  LtBF.ItemIndex := StrToInt(ListBox2.Items[ListBox1.ItemIndex]);
+  if LtAF.Items.Count > LtBF.ItemIndex then begin
+    LtAF.ItemIndex := StrToInt(ListBox2.Items[ListBox1.ItemIndex]);
+  end;
+end;
+
+procedure TForm1.SpeedButton5Click(Sender: TObject);
+begin
+  Clipboard.AsText := LtAF.Items.Text;
+end;
+
+procedure TForm1.SpeedButton6Click(Sender: TObject);
+begin
+  LtAF.Items.SaveToFile(g_saFileName);
+end;
+
+//下層フォルダ以下すべてのファイルの検索
+procedure pSearchFiles(var sl: TStringList; saFolder: AnsiString);
+var
+  iFound : integer;
+  SearchRec : TSearchRec;
+begin
+  iFound := FindFirst(saFolder + '*.*', faAnyFile, SearchRec);
+  while iFound = 0 do begin
+    if FileExists(saFolder + SearchRec.Name) then
+      sl.Add(saFolder + SearchRec.Name);
+
+    //検索したファイルが下層フォルダならば下層フォルダへ
+    if (faDirectory and SearchRec.Attr > 0) and (SearchRec.Name[1] <> '.') then
+      pSearchFiles(sl, saFolder + SearchRec.Name + '\');
+
+    iFound := FindNext(SearchRec);
+  end;
+  FindClose(SearchRec);
+end;
+
+procedure TForm1.SpeedButton7Click(Sender: TObject);
+var
+  i, iFileCount : integer;
+  hDrop : THandle;
+  szFile : array[0..MAX_PATH - 1] of Char;
+  saFolder: AnsiString;
+  sl: TStringList;
+begin
+  if Clipboard.HasFormat(CF_HDROP) then begin
+    LtFiles.Items.Clear;
+
+    hDrop := Clipboard.GetAsHandle(CF_HDROP);
+    iFileCount := DragQueryFile(hDrop, $FFFFFFFF, szFile, MAX_PATH);
+    DragQueryFile(hDrop, 0, szFile, MAX_PATH);
+
+    saFolder := szFile;
+    if DirectoryExists(saFolder) then begin
+      sl := TStringList.Create;
+      try
+        pSearchFiles(sl, saFolder + '\');
+
+        for i := 0 to Pred(sl.Count) do begin
+          g_saFileName := sl[i];
+          LtBF.Items.LoadFromFile(g_saFileName);
+          if LtBF.Items.Count > 0 then begin
+            SpeedButton2Click(SpeedButton2);
+
+            if ListBox1.Items.Count > 0 then begin
+              LtFiles.Items.Add(g_saFileName);
+            end;
+          end;
+        end;
+      finally
+        sl.Free;
+      end;
+    end;
+
+    g_saFileName := '';
+  end;
+end;
+
+procedure TForm1.LtFilesClick(Sender: TObject);
+begin
+  g_saFileName := LtFiles.Items[LtFiles.ItemIndex];
+  LtBF.Items.LoadFromFile(g_saFileName);
+end;
+
+end.
